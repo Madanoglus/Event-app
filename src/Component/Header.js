2c88ec56d96d62b81b59b505874aaf79c9d0e9ce
Mustafa Halil MADANOGLU <mustafahalilmadanoglu@gmail.com>
2022-06-20 12:53:16 +0300

    created db.json

---
+++ src/Component/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Hedaer extends Component {
+  render() {
+    return (
+      <div>
+        <h1>MADAN</h1>
+      </div>
+    )
+  }
+}
+
+export default Hedaer
